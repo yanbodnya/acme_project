d358397ddac57790e396d5e8b9c74240acfd10b5
Yan Bodnya <yanbodnya@gmail.com>
2023-09-23 17:37:09 +0100

    Add birthday calculator form response

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 def birthday(request):
-    form = BirthdayForm()
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
-    return render(request, 'birthday/birthday.html', context=context)
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context) 
